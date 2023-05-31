4b106235d60021a1ff76d5ec836d77c9090976e3
lucas-Jose-Garcia <lucasjosegarcia01@gmail.com>
2023-05-31 20:33:09 -0300

    Ajustes de responsividade

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,8 @@ import plus from './assets/plus.svg'
 import { EmptyTasks } from './components/EmptyTasks'
 import { InfoTasks } from './components/InfoTasks'
 import { Task } from './components/Task'
-import { useState } from 'react'
+import { v4 as uuidv4 } from 'uuid';
+import { useState, useEffect, ChangeEvent, FormEvent } from 'react'
 
 export interface Tasks {
   id: string;
@@ -14,26 +15,37 @@ export interface Tasks {
 }
 
 export function App() {
-  const [tasks, setTasks] = useState([
-    {
-      id: "AB560594",
-      content: "Criar um aplicativo de ToDo",
+  const [tasks, setTasks] = useState<Tasks[] | []>([])
+
+  const [inputValue, setInputValue] = useState('')
+
+  function handleInputChange(event: ChangeEvent<HTMLInputElement>) {
+    setInputValue(event.target.value)
+  }
+  
+  function handleCreateTask(event: FormEvent) {
+    event.preventDefault()
+
+    const newTask: Tasks = {
+      id: uuidv4(),
+      content: inputValue,
       completed: false
-    },
-    {
-      id: "AB560556948i5484",
-      content: "Estudar JavaScript",
-      completed: true
     }
-  ])
+    const newTasks = [newTask, ...tasks]
+
+    setTasks(newTasks)
+    saveTasks(newTasks)
+  }
 
   function handleCheckboxChange(id: string) {
 
-    const newTasks = tasks.map(task => {
+    const previousTasks = tasks.map(task => {
       return task.id !== id ? task : {id: task.id, content: task.content, completed: !task.completed}
     })
 
+    const newTasks = organizeTasks(previousTasks)
     setTasks(organizeTasks(newTasks))
+    saveTasks(newTasks)
   }
 
   function handleDeleteTask(id: string) {
@@ -42,6 +54,14 @@ export function App() {
     })
 
     setTasks(newTasks)
+  }
+
+  function saveTasks(tasks: Tasks[]) {
+    localStorage.setItem('@tudo:tasks', JSON.stringify(tasks));
+  }
+
+  function getTasks() {
+    return localStorage.getItem('@tudo:tasks');
   }
 
   function organizeTasks(tasks: Tasks[]) {
@@ -59,6 +79,12 @@ export function App() {
     return tasks.filter(task => task.completed === true).length
   }
 
+  useEffect(() => {
+    const response = getTasks()
+    const previousTasks = response ? JSON.parse(response) : [];
+    setTasks(previousTasks)
+  }, [])
+
   return (
     <div>
       <header className={styles.header}>
@@ -70,9 +96,9 @@ export function App() {
       </header>
 
       <main className={styles.content}>
-        <form className={styles.taskFrom}>
-          <input placeholder="Adicione uma nova tarefa"/>
-          <button>
+        <form onSubmit={handleCreateTask} className={styles.taskFrom}>
+          <input placeholder="Adicione uma nova tarefa" value={inputValue} onChange={handleInputChange}/>
+          <button type="submit">
             Criar
             <img src={plus}/>
           </button>
@@ -86,13 +112,17 @@ export function App() {
           {tasks.length === 0 ? <EmptyTasks /> : (
             tasks.map(task => {
               return (
-                <Task key={task.id} task={task} onCheckboxChange={handleCheckboxChange} onDeleteTask={handleDeleteTask} />
+                <Task 
+                  key={task.id}
+                  task={task}
+                  onCheckboxChange={handleCheckboxChange}
+                  onDeleteTask={handleDeleteTask}
+                />
               )
             })
           )}
         </section>
       </main>
-
 
 
     </div>
--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -12,7 +12,7 @@ export function Task({task, onCheckboxChange, onDeleteTask}: TasksProps) {
     return (
         <div key={task.id} className={task.completed ? `${styles.task} ${styles.completed}` : styles.task}>
             <div>
-                <input type="checkbox" checked={task.completed} onClick={() => onCheckboxChange(task.id)}/>
+                <input type="checkbox" defaultChecked={task.completed} onClick={() => onCheckboxChange(task.id)}/>
                 <p>{task.content}</p>   
             </div>
             <button>
